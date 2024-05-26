6a27c6d2c976a60ea67d2d03639cbaa69a3289e4
rakesh 09 <rakeshmr9999@gmail.com>
2024-05-26 19:54:48 +0530

    hyper parameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -47,13 +47,56 @@ class ModelTrainer:
                 }
             
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                              models=models,param=params)
 
             ##to get best model score from dict
             best_model_score = max(sorted(model_report.values()))
+            print('best_model_score',best_model_score )
 
             best_model_name=list(model_report.keys())[list(model_report.values()).index(best_model_score)]
-
+            print("model_report.keys()", model_report.keys())
+            print("model_report.values()", model_report.values())
+            print("list_index(best_model_score)", [list(model_report.values()).index(best_model_score)])
+            print("best_model_name ", best_model_name)
             best_model=models[best_model_name]
 
             if best_model_score <0.6:
